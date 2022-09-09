0fe811c06f75c32b788a361836a0e48eaaae61b0
RafaelHernandezVg <112970957+RafaelHernandezVg@users.noreply.github.com>
2022-09-09 09:29:17 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ package com.solera;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo :DDDDDDD");
 	}
 
 }
